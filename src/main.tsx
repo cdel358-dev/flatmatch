68ed87b44b2a18eff4e9211e2b32e5fcaa9e83fa
Christever del Rosario <christever@outlook.com>
2025-08-11 07:28:29 +1200

    Add favicon support and update HTML structure; refine router configuration for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import SearchResults from './pages/SearchResults';
 import Info from './pages/Info';
 import Messages from './pages/Messages';
 import ListingDetails from './pages/ListingDetails';
-import NotesPage from "./pages/NotesPage";
+import NotesPage from './pages/NotesPage';
 import Reviews from './pages/Reviews';
 import FlatmateProfile from './pages/FlatmateProfile';
 import UserProfile from './pages/UserProfile';
@@ -25,37 +25,42 @@ import { ListingsProvider } from './state/ListingsContext';
 import { NotesProvider } from './state/NotesContext';
 import AppErrorBoundary from './components/AppErrorBoundary';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: (
+        <ListingsProvider>
+          <NotesProvider>
+            <App />
+          </NotesProvider>
+        </ListingsProvider>
+      ),
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'search', element: <SearchResults /> },
+        { path: 'about', element: <About /> },
+        { path: 'info', element: <Info /> },
+        { path: 'messages', element: <Messages /> },
+        { path: 'profile', element: <UserProfile /> },
+        { path: 'listing/:id', element: <ListingDetails /> },
+        { path: 'listing/:id/notes', element: <NotesPage /> },
+        { path: 'listing/:id/reviews', element: <Reviews /> },
+        { path: 'listing/:id/flatmate/:mid', element: <FlatmateProfile /> },
+        { path: 'user/:uid', element: <UserProfile /> },
+        { path: 'list-room', element: <ListRoom /> },
+        { path: 'bookmarks', element: <Bookmarks /> },
+        { path: 'info/glossary', element: <Glossary /> },
+        { path: 'info/etiquette', element: <Etiquette /> },
+        { path: 'info/suburbs', element: <Suburbs /> },
+        { path: 'info/tenancy-checklist', element: <TenancyChecklist /> },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: (
-      <ListingsProvider>
-        <NotesProvider>
-          <App />
-        </NotesProvider>
-      </ListingsProvider>
-    ),
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'search', element: <SearchResults /> },
-      { path: 'about', element: <About /> },
-      { path: 'info', element: <Info /> },
-      { path: 'messages', element: <Messages /> },
-      { path: 'profile', element: <UserProfile /> },
-      { path: 'listing/:id', element: <ListingDetails /> },
-      { path: 'listing/:id/notes', element: <NotesPage /> },
-      { path: 'listing/:id/reviews', element: <Reviews /> },
-      { path: 'listing/:id/flatmate/:mid', element: <FlatmateProfile /> },
-      { path: 'user/:uid', element: <UserProfile /> },
-      { path: 'list-room', element: <ListRoom /> },
-      { path: 'bookmarks', element: <Bookmarks /> },
-      { path: 'info/glossary', element: <Glossary /> },
-      { path: 'info/etiquette', element: <Etiquette /> },
-      { path: 'info/suburbs', element: <Suburbs /> },
-      { path: 'info/tenancy-checklist', element: <TenancyChecklist /> },
-    ],
-  },
-]);
+    basename: '/flatmatch', // <-- important for GitHub Pages project sites
+  }
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
